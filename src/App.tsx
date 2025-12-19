293ccb1eabd137b82fbca86f7b4811c609c08198
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2025-12-19 16:41:54 +0700

    initializing the project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "./components/theme-provider";
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div>application</div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
