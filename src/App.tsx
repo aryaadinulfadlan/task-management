8d4b6a79fe66a0e27e75be240795d306c234ccad
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2025-12-20 22:53:25 +0700

    configuring page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { Route, Routes } from "react-router";
+import Header from "./components/header";
 import { ThemeProvider } from "./components/theme-provider";
+import LandingPage from "./pages/public/landing";
+import HomePage from "./pages/protected/home";
+import ProtectedRoute from "./components/protected-route";
+import PublicRoute from "./components/public-route";
+import LoginPage from "./pages/public/login";
+import RegisterPage from "./pages/public/register";
+import NotFoundPage from "./pages/not-found";
+import ProtectedLayout from "./components/protected-layout";
+import TodosPage from "./pages/protected/todos";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div>application</div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<ProtectedLayout />}>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/todos" element={<TodosPage />} />
+          </Route>
+        </Route>
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </ThemeProvider>
   );
 }
---
+++ src/pages/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <div className="min-h-[calc(100vh-2.5rem)] md:min-h-[calc(100vh-3.5rem)] flex justify-center items-center text-3xl lg:text-5xl font-bold px-4 text-center">
+      404 - Page Not Found
+    </div>
+  );
+}
